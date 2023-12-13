7a45010f568526401fbe17e8f606b396838104d1
Shashank <shashankyadav4858@gmail.com>
2023-12-13 13:29:02 +0000

    Changed fetched

--- haskell-project/src/fetch.hs
+++ haskell-project/src/fetch.hs
@@ -1,17 +1,21 @@
---function to fetch weather data from openweathermap.org in haskell
+{-# LANGUAGE OverloadedStrings #-}
+
 module Fetch where
 
-import Network.HTTP.Conduit (simpleHttp)
-import Data.ByteString.Lazy as B
+import Network.HTTP.Conduit (simpleHttp, HttpException)
+import qualified Data.ByteString.Lazy as B
 import Data.Aeson (eitherDecode, FromJSON)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Except (runExceptT, ExceptT, throwError)
+import Control.Exception (try)
 
--- secert key, only 1000 calls per month
+key :: String
 key = "ff3e5ba7f0131d75e2e2b74eead4f697"
--- This is the function to be called from Main.hs.
-fetchWeatherData :: String  -> ExceptT String IO B.ByteString
+
+fetchWeatherData :: String -> ExceptT String IO B.ByteString
 fetchWeatherData location = do
     let apiUrl = "http://api.weatherstack.com/current?access_key=" ++ key ++ "&query=" ++ location
-    response <- liftIO $ simpleHttp apiUrl
-    return response
+    response <- liftIO $ try (simpleHttp apiUrl) :: ExceptT String IO (Either HttpException B.ByteString)
+    case response of
+        Right body -> return body
+        Left err -> throwError $ "HTTP request failed: " ++ show err
