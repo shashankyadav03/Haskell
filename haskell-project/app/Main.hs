1c04b150e88f564de8be346bbe35cdf1be9c8dcf
Sloan <sloand726@gmail.com>
2023-12-15 13:17:46 +0000

    Made changes to capitalize arg command

--- haskell-project/app/Main.hs
+++ haskell-project/app/Main.hs
@@ -12,13 +12,23 @@ import Data.Time.Clock
 import Data.Time.Format
 import Database (connectDB, getDbTime)
 import Control.Monad (join)
+import Data.Char (toUpper, toLower)
+import Data.List (intercalate)
 
 -- Function get location
 getLocation :: [String] -> String
 getLocation args = case args of
     [] -> "London"  -- Default to London if no arguments are provided
-    [loc] -> loc    
+    [loc] -> capitalizeWords loc   
     _ -> "\nUsage: WeatherWander <LOCATION>"
+
+-- Function to capitalize each word in a string
+capitalizeWords :: String -> String
+capitalizeWords = intercalate " " . map capitalizeWord . words
+  where
+    capitalizeWord :: String -> String
+    capitalizeWord [] = []
+    capitalizeWord (x:xs) = toUpper x : map toLower xs
 
 -- Function to get Data from API
 fetchDataAndProcess :: String -> IO ()
